c4a1edfd55a91131fd167f22569d3915e9b33e94
simoncoessens <56151036+simoncoessens@users.noreply.github.com>
2023-12-13 19:00:40 +0100

    Update stream.py

--- src/7. Streaming Bonus Task/stream.py
+++ src/7. Streaming Bonus Task/stream.py
@@ -4,7 +4,14 @@ import pandas as pd  # read csv, df manipulation
 import time  # to simulate a real time data, time loop
 import plotly.express as px  # interactive charts
 from sqlalchemy import create_engine
+from datetime import datetime
 
+
+st.set_page_config(
+    page_title='Real-Time SNCB Dashboard',
+    page_icon='✅',
+    layout='wide'
+)
 
 # Database connection parameters
 dbname = 'DataMining'
@@ -17,42 +24,44 @@ port = '5433'  # default PostgreSQL port is 5432
 connection_str = f"postgresql://{user}:{password}@{host}:{port}/{dbname}"
 engine = create_engine(connection_str)
 
-# Define the time frame for the query
-start_time = '2023-01-01 00:00:00'  # Replace with your start time
-end_time = '2023-01-30 23:59:59'    # Replace with your end time
+# Title for the date selection section
+st.title("Select Date Range")
 
-# Define the query with placeholders for time frame parameters
-query_time_frame = f"""
-SELECT *
-FROM vehicle_data
-WHERE timestamps_UTC BETWEEN '{start_time}' AND '{end_time}'
-ORDER BY timestamps_UTC ASC;
-"""
+# Create two date input widgets for start and end dates
+start_time = st.date_input("Start Date", value=datetime(2023, 1, 1))
+end_time = st.date_input("End Date", value=datetime(2023, 1, 31))
 
-# Execute the query and store the results in a DataFrame
-df = pd.read_sql_query(query_time_frame, engine)
+# You can add a check to ensure the start date is not after the end date
+if start_time > end_time:
+    st.error("Error: End date must fall after start date.")
 
+# Display the selected date range
+st.write(f"Selected date range: {start_time.strftime('%Y-%m-%d')} to {end_time.strftime('%Y-%m-%d')}")
 
-st.set_page_config(
-    page_title='Real-Time SNCB Dashboard',
-    page_icon='✅',
-    layout='wide'
-)
 
 # dashboard title
 
-st.title("Real-Time / Live Data Science Dashboard")
+st.title("Streaming SNCB Dashboard")
 
 # top-level filters
 
-train_filter = st.selectbox("Select the Train id", pd.unique(df['mapped_veh_id']))
+train_filter = st.selectbox("Select the Train id", list(range(102, 198)))
 
 # creating a single-element container.
 placeholder = st.empty()
 
 # dataframe filter
+# Define the query with placeholders for time frame parameters
+query_time_frame = f"""
+SELECT *
+FROM vehicle_data
+WHERE timestamps_UTC BETWEEN '{start_time}' AND '{end_time}'
+AND mapped_veh_id = '{train_filter}'
+ORDER BY timestamps_UTC ASC;
+"""
 
-df = df[df['mapped_veh_id'] == train_filter]
+# Execute the query and store the results in a DataFrame
+df = pd.read_sql_query(query_time_frame, engine)
 
 # near real-time / live feed simulation
 live_df = pd.DataFrame(columns=df.columns)
