250bcb4350aafe473e93669df4972200d4d77587
simoncoessens <56151036+simoncoessens@users.noreply.github.com>
2023-12-14 01:35:20 +0100

    research + bonus

--- src/7. Streaming Bonus Task/stream.py
+++ src/7. Streaming Bonus Task/stream.py
@@ -4,12 +4,13 @@ import pandas as pd  # read csv, df manipulation
 import time  # to simulate a real time data, time loop
 import plotly.express as px  # interactive charts
 from sqlalchemy import create_engine
-from datetime import datetime
+from datetime import datetime, time as dt_time
+import humanize
 
 
 st.set_page_config(
     page_title='Real-Time SNCB Dashboard',
-    page_icon='✅',
+    page_icon='🚆',
     layout='wide'
 )
 
@@ -28,8 +29,17 @@ engine = create_engine(connection_str)
 st.title("Select Date Range")
 
 # Create two date input widgets for start and end dates
-start_time = st.date_input("Start Date", value=datetime(2023, 1, 1))
-end_time = st.date_input("End Date", value=datetime(2023, 1, 31))
+# Create two date input widgets for start and end dates
+start_date = st.date_input("Start Date", value=datetime(2023, 1, 1))
+end_date = st.date_input("End Date", value=datetime(2023, 1, 31))
+
+# Create two time input widgets for start and end times
+start_time = st.time_input("Start Time", value=dt_time(0, 0))
+end_time = st.time_input("End Time", value=dt_time(23, 59))
+
+# Combine date and time into datetime objects
+start_datetime = datetime.combine(start_date, start_time)
+end_datetime = datetime.combine(end_date, end_time)
 
 # You can add a check to ensure the start date is not after the end date
 if start_time > end_time:
@@ -38,14 +48,12 @@ if start_time > end_time:
 # Display the selected date range
 st.write(f"Selected date range: {start_time.strftime('%Y-%m-%d')} to {end_time.strftime('%Y-%m-%d')}")
 
-
+train_filter = st.selectbox("Select the Train id", list(range(102, 198)))
 # dashboard title
 
-st.title("Streaming SNCB Dashboard")
+st.title("Streaming SNCB Dashboard 🚆")
 
 # top-level filters
-
-train_filter = st.selectbox("Select the Train id", list(range(102, 198)))
 
 # creating a single-element container.
 placeholder = st.empty()
@@ -55,7 +63,7 @@ placeholder = st.empty()
 query_time_frame = f"""
 SELECT *
 FROM vehicle_data
-WHERE timestamps_UTC BETWEEN '{start_time}' AND '{end_time}'
+WHERE timestamps_UTC BETWEEN '{start_datetime}' AND '{end_datetime}'
 AND mapped_veh_id = '{train_filter}'
 ORDER BY timestamps_UTC ASC;
 """
@@ -65,35 +73,119 @@ df = pd.read_sql_query(query_time_frame, engine)
 
 # near real-time / live feed simulation
 live_df = pd.DataFrame(columns=df.columns)
+# Initialize a DataFrame to keep track of boundary crossings
+boundary_log_df = pd.DataFrame(columns=['timestamp', 'sensor', 'value', 'duration', 'boundary'])
+
+# Initialize a DataFrame to keep track of boundary crossings
+boundary_log_df = pd.DataFrame(columns=['start_timestamp', 'sensor', 'value', 'duration', 'boundary'])
+
+# Dictionary to track if a sensor is currently above its boundary
+boundary_status = {sensor: {'crossed': False, 'start_time': None} for sensor in ['rs_e_inairtemp_pc1', 'rs_e_inairtemp_pc2',
+                                                                                 'rs_e_wattemp_pc1', 'rs_e_wattemp_pc2',
+                                                                                 'rs_e_oilpress_pc1', 'rs_e_oilpress_pc2']}
+
+# Boundary checks:
+ # Check if any sensor value exceeds its boundary and log the incident
+sensor_boundaries = {'rs_e_inairtemp_pc1': 65, 'rs_e_inairtemp_pc2': 65,
+                         'rs_e_wattemp_pc1': 100, 'rs_e_wattemp_pc2': 100,
+                         'rs_e_oilpress_pc1': 115, 'rs_e_oilpress_pc2': 115}
 
 for index, row in df.iterrows():
     # add row to the current df
     live_df = pd.concat([live_df, pd.DataFrame([row])], ignore_index=True)
 
     # creating KPIs
-    avg_temp_air = live_df['rs_e_inairtemp_pc1'].mean()  # Average air temperature
-    avg_temp_water = live_df['rs_e_wattemp_pc1'].mean()  # Average water temperature
-    avg_temp_oil = live_df['rs_e_oilpress_pc1'].mean()  # Average oil temperature
+    air_1 = live_df['rs_e_inairtemp_pc1'].iloc[-1]
+    air_2 = live_df['rs_e_inairtemp_pc2'].iloc[-1]
+    water_1 = live_df['rs_e_wattemp_pc1'].iloc[-1]
+    water_2 = live_df['rs_e_wattemp_pc2'].iloc[-1]
+    oil_1 = live_df['rs_e_oilpress_pc1'].iloc[-1]
+    oil_2 = live_df['rs_e_oilpress_pc2'].iloc[-1]
+
+    # Boundary checks:
+
+    current_time = row['timestamps_utc']  # Replace with your actual timestamp column
+
+    for sensor, boundary in sensor_boundaries.items():
+        sensor_value = row[sensor]
+        if sensor_value > boundary:
+            if not boundary_status[sensor]['crossed']:
+                # Sensor has just crossed the boundary
+                boundary_status[sensor]['crossed'] = True
+                boundary_status[sensor]['start_time'] = current_time
+                boundary_log_df = boundary_log_df.append({
+                    'start_timestamp': current_time,
+                    'sensor': sensor,
+                    'value': sensor_value,
+                    'duration': 0,  # Initialize duration to 0
+                    'boundary': boundary
+                }, ignore_index=True)
+            else:
+                # Update the duration in minutes
+                duration_minutes = (current_time - boundary_status[sensor]['start_time']).total_seconds() / 60
+                boundary_log_df.at[
+                    boundary_log_df[boundary_log_df['sensor'] == sensor].index[-1], 'duration'] = duration_minutes
+        else:
+            # Sensor value has gone below the boundary
+            boundary_status[sensor]['crossed'] = False
+            boundary_status[sensor]['start_time'] = None
 
     with placeholder.container():
-        # create three columns
-        kpi1, kpi2, kpi3 = st.columns(3)
+        # Assuming live_df['timestamps_utc'].iloc[-1] is a datetime object
+        time_value = live_df['timestamps_utc'].iloc[-1]
+        formatted_time = time_value.strftime("%A, %d %B %Y %H:%M:%S")
+        relative_time = humanize.naturaltime(datetime.now() - time_value)
 
-        # fill in those three columns with respective metrics or KPIs
-        kpi1.metric(label="Air temperature ", value=round(avg_temp_air))
-        kpi2.metric(label="Oil temperature", value=round(avg_temp_oil))
-        kpi3.metric(label="Water temperature", value=round(avg_temp_water))
-
+        kpi1, kpi2, kpi3, kpi4 = st.columns(4)
+        kpi1.metric(label="Air Temperature", value=f"{round(air_1)}°C")
+        kpi2.metric(label="Water Temperature", value=f"{round(water_1)}°C")
+        kpi3.metric(label="Oil Temperature", value=f"{round(oil_1)}°C")
+        kpi4.metric(label="Time", value=formatted_time, delta=relative_time)
 
         # create two columns for charts
 
-        fig_col1, fig_col2 = st.columns(2)
+        fig_col1, fig_col2, fig_col3 = st.columns(3)
+
         with fig_col1:
-            st.markdown("### Average Air Temperature Over Time")
-            fig = px.line(live_df, y='rs_e_inairtemp_pc1')
-            st.write(fig)
+            st.markdown("### Air Temperature Over Time")
+            # Plotting air temperature from both sensors on the same graph
+            fig_air = px.line(live_df, x='timestamps_utc', y=['rs_e_inairtemp_pc1', 'rs_e_inairtemp_pc2'],
+                              labels={'value': 'Temperature (°C)', 'variable': 'Sensor'},
+                              title='Air Temperature Over Time')
+            fig_air.update_layout(legend_title_text='Sensor', xaxis_title='Time', yaxis_title='Air Temperature (°C)')
+            # Adding maximum acceptable temperature line for air
+            fig_air.add_hline(y=65, line_dash="dot", line_color="red", annotation_text="Max Acceptable Temp",
+                              annotation_position="bottom right")
+            st.plotly_chart(fig_air)
+
         with fig_col2:
-            st.markdown("### Location from the Most Recent Data Entries")
+            st.markdown("### Water Temperature Over Time")
+            # Plotting water temperature from both sensors on the same graph
+            fig_water = px.line(live_df, x='timestamps_utc', y=['rs_e_wattemp_pc1', 'rs_e_wattemp_pc2'],
+                                labels={'value': 'Water Temperature (°C)', 'variable': 'Sensor'},
+                                title='Water Temperature Over Time')
+            fig_water.update_layout(legend_title_text='Sensor', xaxis_title='Time',
+                                    yaxis_title='Water Temperature (°C)')
+            # Adding maximum acceptable temperature line for water
+            fig_water.add_hline(y=100, line_dash="dot", line_color="red", annotation_text="Max Acceptable Temp",
+                                annotation_position="bottom right")
+            st.plotly_chart(fig_water)
+
+        with fig_col3:
+            st.markdown("### Oil Pressure Over Time")
+            # Plotting oil pressure from both sensors on the same graph
+            fig_oil = px.line(live_df, x='timestamps_utc', y=['rs_e_oilpress_pc1', 'rs_e_oilpress_pc2'],
+                              labels={'value': 'Oil Pressure', 'variable': 'Sensor'},
+                              title='Oil Pressure Over Time')
+            fig_oil.update_layout(legend_title_text='Sensor', xaxis_title='Time', yaxis_title='Oil Pressure')
+            # Adding maximum acceptable temperature line for oil
+            fig_oil.add_hline(y=115, line_dash="dot", line_color="red", annotation_text="Max Acceptable Temp",
+                              annotation_position="bottom right")
+            st.plotly_chart(fig_oil)
+
+        fig_col1_r2, fig_col2_r2, fig_col3_r2 = st.columns(3)
+        with fig_col1_r2:
+            st.markdown("### Location")
 
             # Select the last three entries of the live DataFrame
             last_entries = live_df.tail(3)
@@ -103,7 +195,7 @@ for index, row in df.iterrows():
                 # Create a map with the last three points and connect them with a line
                 map_fig = px.line_mapbox(
                     last_entries, lat='lat', lon='lon',
-                    zoom=10, height=300,
+                    zoom=12, height=300,
                     color_discrete_sequence=["red"]  # Set the line color to red
                 )
 
@@ -112,7 +204,7 @@ for index, row in df.iterrows():
                     lon=last_entries['lon'],
                     lat=last_entries['lat'],
                     mode='markers',
-                    marker=dict(size=10, color='blue'),  # Change size and color as needed
+                    marker=dict(size=30, color='blue'),  # Change size and color as needed
                 )
 
                 # Update the layout of the map
@@ -124,7 +216,8 @@ for index, row in df.iterrows():
             else:
                 st.write("Not enough location data available for plotting.")
 
-        st.markdown("### Detailed Data View")
-        st.dataframe(df)
-        time.sleep(1)
+        with fig_col2_r2:
+            st.markdown("### Boundary crossings:")
+            st.dataframe(boundary_log_df)
+        time.sleep(0.1)
     # placeholder.empty()
